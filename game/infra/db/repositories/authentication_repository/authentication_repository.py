365a0120733aa37716c159afd83c89ca225453e7
Daniel Silva Ramos <daniel.silvaramos.93@gmail.com>
2023-12-01 17:04:08 -0300

    modified:   game/infra/db/repositories/authentication_repository/authentication_repository.py

--- game/infra/db/repositories/authentication_repository/authentication_repository.py
+++ game/infra/db/repositories/authentication_repository/authentication_repository.py
@@ -1,4 +1,4 @@
-from uuid import UUID
+from uuid import UUID, uuid1
 from sqlalchemy import insert, select, update
 from game.data.interfaces.authentication_repository_interface import (
     AuthenticationRepositoryInterface,
@@ -16,25 +16,16 @@ class AuthenticationRepository(AuthenticationRepositoryInterface):
             engine = db.get_engine()
             UsersEntity.create_table(engine)
             AuthenticationEntity.create_table(engine=engine)
-            query = (
-                update(AuthenticationEntity)
-                .filter(AuthenticationEntity.user_id == user_id)
-                .values(active=False)
-            )
-            inse = insert(AuthenticationEntity).values(user_id=user_id).returning(AuthenticationEntity)
-            db.session.execute(query)
+            inse = insert(AuthenticationEntity).values(user_id=user_id, token=uuid1()).returning(AuthenticationEntity)
             new_data = db.session.execute(inse).scalar()
             try:
                 db.session.commit()
                 db.session.refresh(new_data)
-                db.session.reset()
-                # db.session.expire(new_data)
             except Exception as exception:
                 breakpoint()
                 db.session.rollback()
                 raise exception
         return new_data
-        # return ret
 
     @classmethod
     def get_token(cls, user_id: int) -> AuthenticationModel:
